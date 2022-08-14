645c2ca538b34cb28f320df2dd120f32248a6e02
Adriaan Boshoff <adriaanboshoff25@gmail.com>
2022-08-14 14:51:15 +0200

    Base UI design

--- SellixDemo.dpr
+++ SellixDemo.dpr
@@ -3,7 +3,10 @@ program SellixDemo;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  ufrmMain in 'ufrmMain.pas' {frmMain};
+  ufrmMain in 'ufrmMain.pas' {frmMain},
+  Sellix.Types in 'API\Sellix.Types.pas',
+  Sellix in 'API\Sellix.pas',
+  Sellix.Consts in 'API\Sellix.Consts.pas';
 
 {$R *.res}
 
--- ufrmMain.pas
+++ ufrmMain.pas
@@ -4,10 +4,14 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.TabControl;
 
 type
   TfrmMain = class(TForm)
+    tlbHeader: TToolBar;
+    statFooter: TStatusBar;
+    tbcMain: TTabControl;
   private
     { Private declarations }
   public
