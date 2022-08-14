fe9f7f4b24e08f88cf6cb42b3ea6ef7040b32523
Adriaan Boshoff <adriaanboshoff25@gmail.com>
2022-08-14 15:13:27 +0200

    Create Blacklist Object and Enums

--- API/Sellix.Types.pas
+++ API/Sellix.Types.pas
@@ -2,6 +2,27 @@ unit Sellix.Types;
 
 interface
 
+// Blacklist Types ===================================
+// https://developers.sellix.io/#blacklist-object
+type
+  TSellixBlacklistScope = (bcPRIVATE, bcSHARED);
+
+type
+  TSellixBlacklistType = (btEMAIL, btIP, btCOUNTRY, btISP, btASN, btHOST);
+
+type
+  TSellixBlacklist = class
+    UniqID: string;
+    Scope: TSellixBlacklistScope;
+    ShopID: Integer;
+    Data: string;
+    Note: string;
+    CreatedAt: Int64;
+    UpdatedAT: Int64;
+    UpdatedBy: Integer;
+  end;
+  // =================================================
+
 implementation
 
 end.
